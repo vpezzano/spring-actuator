238c0e7573716184b485ad27f385243354a63337
unknown <valentino_pezzano-github@yahoo.com>
2018-12-04 22:00:39 +0100

    Added hook into Spring Boot lifecycle.

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://www.javacodegeeks.com/2018/11/spring-boot-actuator-tutorial.html
 
 Also, it contains examples of using the @Lazy annotation, as described here:
 https://www.javacodegeeks.com/2018/03/spring-lazy-annotation-use-cases.html
+
+Finally, the application also shows how to hook into Spring Boot lifecycle, discussed here:
+https://www.javacodegeeks.com/2018/11/running-code-spring-boot-startup.html
--- src/main/java/com/javacodegeeks/war/InitDemoWarInitializer.java
+++ src/main/java/com/javacodegeeks/war/InitDemoWarInitializer.java
@@ -8,6 +8,7 @@ import com.javacodegeeks.SpringActuatorApplication;
 public class InitDemoWarInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		System.out.println("SpringApplicationBuilder: configure invoked.");
 		SpringActuatorApplication.addInitHooks(builder.application());
 		return builder.sources(SpringActuatorApplication.class);
 	}
